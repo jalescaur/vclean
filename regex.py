f2d39e51403cf43d4aacc1c0e4b9b6b90f23664d
jales caur <110789974+jalescaur@users.noreply.github.com>
2025-07-01 13:12:55 -0300

    Update stoplist and regex

--- regex.py
+++ regex.py
@@ -4,6 +4,6 @@ def clean_text(text):
     """
     Função para remover risadas do texto.
     """
-    regex = r'(?i)\b(?:k{1,}|hah?a+h?a+|a?[a-z]*(?:h[ae]|k[ae]|u[ae])+[a-z]*)\b'
+    regex = r'(?i)(?<!\w)\b(?:k{1,}|hah?a+h?a+|a?[a-z]*(?:h[ae]|k[ae]|u[ae])+[a-z]*)\b|(?:[a-zA-Z][áéíóúàèìòùâêîôûãõäëïöüñç]|[áéíóúàèìòùâêîôûãõäëïöüñç]|\b[a-zA-Z]\d|\d[a-zA-Z]\b)(?!\w)'
     return re.sub(regex, '', text)
 
