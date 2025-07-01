722bd3ea62b146c39c5ecd868d4c9fce2236de15
jales caur <110789974+jalescaur@users.noreply.github.com>
2025-07-01 13:16:41 -0300

    Update regex.py

--- regex.py
+++ regex.py
@@ -4,6 +4,21 @@ def clean_text(text):
     """
     Função para remover risadas do texto.
     """
-    regex = r'(?i)(?<!\w)\b(?:k{1,}|hah?a+h?a+|a?[a-z]*(?:h[ae]|k[ae]|u[ae])+[a-z]*)\b|(?:[a-zA-Z][áéíóúàèìòùâêîôûãõäëïöüñç]|[áéíóúàèìòùâêîôûãõäëïöüñç]|\b[a-zA-Z]\d|\d[a-zA-Z]\b)(?!\w)'
+    regex = r'''(?ix)
+        (?<!\w)
+        \b
+        (
+            k{4,}                                     # pelo menos 4 'k'
+            |hah?a+h?a+                               # risadas tipo 'haha'
+            |[a-z]{0,}[hku][ae][a-z]{2,}              # sons tipo 'hue', 'kae', com pelo menos 4 caracteres
+            |[a-z]{4,}                                # qualquer palavra com 4+ letras
+            |[a-zA-Z][áéíóúàèìòùâêîôûãõäëïöüñç]       # letras com acento
+            |[áéíóúàèìòùâêîôûãõäëïöüñç]{2,}           # acentuadas com mínimo 2 caracteres
+            |\b[a-zA-Z]\d{3,}                         # letra seguida de 3+ números
+            |\b\d{3,}[a-zA-Z]                         # número seguido de letra
+        )
+        \b
+        (?!\w)
+    '''
     return re.sub(regex, '', text)
 
